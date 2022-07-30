13cd77052b7778d3cdc0206c33bd147c045d205d
suji321 <sujisasi4@gmail.com>
2022-07-30 22:18:10 +0530

    refactorin and classList add/remove

--- script.js
+++ script.js
@@ -6,5 +6,19 @@ const btnCloseModal = document.querySelector('.close-modal')
 const btnOpenModal = document.querySelectorAll('.show-modal')
 console.log(btnOpenModal);
 
+function closeModal() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+function openModal() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
 for (let i = 0; i < btnOpenModal.length; i++)
-    console.log(btnOpenModal[i].textContent)
+    btnOpenModal[i].addEventListener('click', openModal)
+
+btnCloseModal.addEventListener('click', closeModal)
+
+overlay.addEventListener('click', closeModal)
